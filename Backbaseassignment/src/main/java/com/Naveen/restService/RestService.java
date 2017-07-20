6e98d754ee687c1404701b1039354f737268ad7e
naveenkothas <naveenkothas@gmail.com>
2017-07-19 23:32:13 -0500

    rest service changes

--- Backbaseassignment/src/main/java/com/Naveen/restService/RestService.java
+++ Backbaseassignment/src/main/java/com/Naveen/restService/RestService.java
@@ -5,14 +5,40 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import org.apache.log4j.Logger;
+
 @Path("/abc")
 public class RestService {
+	
+	final static Logger logger = Logger.getLogger(RestService.class);
 	
 	@GET
 	@Path("/{param}")
 	public Response getMsg(@PathParam("param") String msg) {
+		
+		logger.debug("getMsg");
 
 		String output = "Jersey say : " + msg;
+
+		return Response.status(200).entity(output).build();
+
+	}
+	
+	@GET
+	@Path("/rbs/accounts/savings-kids-john/public/transactions")
+	public Response getTransactionList() {
+
+		String output = "Jersey say : " ;
+
+		return Response.status(200).entity(output).build();
+
+	}
+	
+	@GET
+	@Path("/rbs/accounts/savings-kids-john/public/transactions1")
+	public Response getTransactionListBasedonFilter() {
+
+		String output = "Jersey say : " ;
 
 		return Response.status(200).entity(output).build();
 
